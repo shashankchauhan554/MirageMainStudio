2406b7b9f86c91c568bc5fca638eedf4021781ed
sambit.9123 <sambit.9123@gmail.com>
2023-09-15 22:37:57 +0530

    this is emailservice

---
+++ Email-Service/src/test/java/com/stackroute/EmailService/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.EmailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
