398a7a8fc69394ee58c35b5221e456f3cd9239b4
keshav <keshav@stackroute.in>
2023-09-18 19:38:08 +0530

    Resolve conflicts

--- Email-Service/src/main/java/com/stackroute/EmailService/model/User.java
+++ Email-Service/src/main/java/com/stackroute/EmailService/model/User.java
@@ -1,14 +1,10 @@
 package com.stackroute.EmailService.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
@@ -16,27 +12,5 @@ public class User {
     private String email;
     private boolean confirmed;
 
-    public User(boolean confirmed) {
-        this.confirmed = confirmed;
-    }
 
-    public User(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isConfirmed() {
-        return confirmed;
-    }
-
-    public void setConfirmed(boolean confirmed) {
-        this.confirmed = confirmed;
-    }
 }
