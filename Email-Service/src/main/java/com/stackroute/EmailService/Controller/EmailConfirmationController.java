8eafdc166c98f3e685ff452f749d19f1eaa4cfd3
sambit.9123 <sambit.9123@gmail.com>
2023-09-19 10:38:14 +0530

    email service added

--- Email-Service/src/main/java/com/stackroute/EmailService/Controller/EmailConfirmationController.java
+++ Email-Service/src/main/java/com/stackroute/EmailService/Controller/EmailConfirmationController.java
@@ -1,63 +1,21 @@
 package com.stackroute.EmailService.Controller;
-import com.stackroute.EmailService.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.stackroute.EmailService.Service.EmailService;
 import com.stackroute.EmailService.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.mail.MessagingException;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
+
 @RestController
 @RequestMapping("/email")
 public class EmailConfirmationController {
-    private final EmailService emailService;
-    private final UserRepository userRepository;
 
     @Autowired
-    public EmailConfirmationController(EmailService emailService, UserRepository userRepository) {
-        this.emailService = emailService;
-        this.userRepository = userRepository;
-    }
+    private EmailService emailService;
+
     @PostMapping("/send-confirmation")
-    public String sendConfirmationEmail(@RequestParam String email) {
-        // Check if the user already exists in the database
-        User user = userRepository.findById(email).orElse(null);
-
-        if (user == null) {
-            // User does not exist, create a new user
-            user = new User(email, false);
-        }
-
-        // Generate a unique confirmation token
-        String confirmationToken = generateConfirmationToken();
-
-        // Send the confirmation email
-        String confirmationLink = "http://miragemanestudio.com/confirm?token=" + confirmationToken;
-        emailService.sendConfirmationEmail(email, confirmationLink);
-
-        // Update the user's confirmation status and save to the database
-        user.setConfirmed(false); // Set to false to indicate unconfirmed email
-        userRepository.save(user);
-
-        // Return a response, indicating that the email has been sent
-        return "Email sent for confirmation.";
-    }
-    private String generateConfirmationToken() {
-        return UUID.randomUUID().toString();
-    }
-    @GetMapping("/confirm")
-    public String confirmEmail(@RequestParam String token) {
-        // Find the user with the corresponding email
-        User user = userRepository.findByToken(token);
-
-        if (user != null) {
-            // Token matches, update the user's email status
-            user.setConfirmed(true);
-            userRepository.save(user);
-
-            // Return a success message
-            return "Email confirmed successfully for user: " + user.getEmail();
-        } else {
-            // If the token is not found or invalid, return an error message
-            return "Invalid confirmation token.";
-        }
+    public void sendConfirmationEmail(@RequestBody User confirmationEmail) throws MessagingException {
+        emailService.sendConfirmationEmail(confirmationEmail);
     }
 }
--- Email-Service/src/main/java/com/stackroute/EmailService/model/User.java
+++ Email-Service/src/main/java/com/stackroute/EmailService/model/User.java
@@ -1,42 +1,19 @@
 package com.stackroute.EmailService.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    @Id
-    private String email;
-    private boolean confirmed;
 
-    public User(boolean confirmed) {
-        this.confirmed = confirmed;
-    }
+    private String to;
+    private String subject;
+    private String body;
 
-    public User(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isConfirmed() {
-        return confirmed;
-    }
-
-    public void setConfirmed(boolean confirmed) {
-        this.confirmed = confirmed;
-    }
 }
