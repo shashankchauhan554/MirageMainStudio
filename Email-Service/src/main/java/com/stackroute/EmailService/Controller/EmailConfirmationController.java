e3624167caac172430ce49d5a345c3c3abac2d39
sambit.9123 <sambit.9123@gmail.com>
2023-09-19 11:34:45 +0530

    email service added

--- Email-Service/src/main/java/com/stackroute/EmailService/Controller/EmailConfirmationController.java
+++ Email-Service/src/main/java/com/stackroute/EmailService/Controller/EmailConfirmationController.java
@@ -5,16 +5,15 @@ import com.stackroute.EmailService.Service.EmailService;
 import com.stackroute.EmailService.model.User;
 import javax.mail.MessagingException;
 import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
 
 @RestController
-@RequestMapping("/email")
+
 public class EmailConfirmationController {
 
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/send-confirmation")
+    @PostMapping("/api/email")
     public void sendConfirmationEmail(@RequestBody User confirmationEmail) throws MessagingException {
         emailService.sendConfirmationEmail(confirmationEmail);
     }
