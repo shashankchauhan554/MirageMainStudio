92672e587926ab1b8439750ad294cc1d11607c69
kommalapatikarthikkumar <kommalapatikarthikkumar@gmail.com>
2023-09-22 11:52:38 +0530

    updated UserService

--- UserService/Exception/AlreadyFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.stackroute.UserService.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT,reason = "trying to add same data")
-public class AlreadyFoundException extends Exception {
-}
--- UserService/Exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.stackroute.UserService.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "There is no User with requested EmailID")
-public class EntityNotFoundException  extends Exception{
-}
