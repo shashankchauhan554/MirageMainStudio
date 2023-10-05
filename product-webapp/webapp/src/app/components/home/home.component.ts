85a6e30410406b131665dfa812ef9efa25daf98b
keshav <keshav@stackroute.in>
2023-10-05 18:19:29 +0530

    navbar fixed

--- product-webapp/webapp/src/app/components/home/home.component.ts
+++ product-webapp/webapp/src/app/components/home/home.component.ts
@@ -15,53 +15,6 @@ import { User } from 'src/app/model/user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
- constructor(private router: Router, private login: UserServiceService, local : LocalstorageservicesService
- ){}
- username: string | undefined;
-​
  
-​
- /*ngOnInit(): void {
-   const currentUser: User | null = this.local.getCurrentUser();
-   if (currentUser) {
-    this.username = currentUser.userName;
-  }
- }*/
-​
-​
-   loginCheck(){
-   this.router.navigateByUrl('login');
-   }
-   profile(){
-     this.router.navigateByUrl('userdashboard');
-   }
-   order(){
-     this.router.navigateByUrl('salon');
-   }
-   
-   subscribe(){
-     this.router.navigateByUrl('subscribe');
-   }
-   register(){
-     this.router.navigateByUrl('signup');
-   }
-   
-   Cart(){
-     this.router.navigateByUrl('myBookings');
-   }
-   logout(){
-    localStorage.removeItem('Token');
-    this.router.navigateByUrl('login');
-   }
-   review(){
-     this.router.navigateByUrl('testt');
-   }
-   bookings(){
-    console.log('hello')
-    this.router.navigate(['booking-form']);
-   }
-   feedback(){
-    this.router.navigateByUrl("feedback");
-   }
 
   }
--- product-webapp/webapp/src/app/header/header.component.ts
+++ product-webapp/webapp/src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { UserServiceService } from '../services/user-service/user-service.service';
+import { LocalstorageservicesService } from 'src/app/services/localstorageservices.service';
 
 @Component({
   selector: 'app-header',
@@ -10,43 +12,57 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private router: Router, private login: UserServiceService, local : LocalstorageservicesService
+    ){}
+    username: string | undefined;
+   ​
+    
+   ​
+    /*ngOnInit(): void {
+      const currentUser: User | null = this.local.getCurrentUser();
+      if (currentUser) {
+       this.username = currentUser.userName;
+     }
+    }*/
+   ​
+   ​
+      loginCheck(){
+      this.router.navigateByUrl('login');
+      }
+      profile(){
+        this.router.navigateByUrl('userdashboard');
+      }
+      order(){
+        this.router.navigateByUrl('salon');
+      }
+      
+      subscribe(){
+        this.router.navigateByUrl('subscribe');
+      }
+      register(){
+        this.router.navigateByUrl('signup');
+      }
+      
+      Cart(){
+        this.router.navigateByUrl('myBookings');
+      }
+      logout(){
+       localStorage.removeItem('Token');
+       this.router.navigateByUrl('login');
+      }
+      review(){
+        this.router.navigateByUrl('testt');
+      }
+      bookings(){
+       console.log('hello')
+       this.router.navigate(['booking-form']);
+      }
+      feedback(){
+       this.router.navigateByUrl("feedback");
+      }
+ 
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver, private router:Router) {}
 
   
-
-  ngOnInit(): void {
-  }
-
-  isActiveRoute(routePath: string): boolean {
-    return this.router.isActive(routePath, true);
-  }
-
-
-  navigateToAdmin(): any {
-
-
-    sessionStorage.removeItem('bearerToken');
-    this.router.navigate(['/admin-content']) ;
-
-  }
-  navigateToCustomer(): any {
-
-    sessionStorage.removeItem('bearerToken');
-    this.router.navigate(['/customer-content']) ;
-
-  }
-
-  onlogout() {
-
-    sessionStorage.removeItem('bearerToken');
-    this.router.navigate(['/salon-content']);
-  }
 
 }
